d26bde4a880077b44f5ac02332ba56a1f3d41b8f
Nick <kburton@relaynetwork.com>
2014-12-01 09:47:09 -0500

    new binding

--- lib/krb-clojure.el
+++ lib/krb-clojure.el
@@ -856,7 +856,34 @@ the pre-existing package statements.
   (interactive)
   (slime-inspect krb-clojure-replay-inspect-expression-expr))
 
-
+(defun krb-clojure-interactive-def-expression (var-name expression)
+  (interactive
+   (list
+    (read-string
+     ;; prompt
+     (concat "Var: ")
+     ;; initial-input
+     (save-excursion
+       (backward-sexp 1)
+       (slime-last-expression))
+     ;; history
+     'krb-clojure-interactive-def-expression-hist
+     ;; default-value
+     "chicken"
+     ;; inherit-input-method
+     t)
+    (read-string
+     ;; prompt
+     (concat "Expression: " (slime-last-expression) ": ")
+     ;; initial-input
+     (slime-last-expression)
+     ;; history
+     'krb-clojure-set-replay-inspect-expression-hist
+     ;; default-value
+     (slime-last-expression)
+     ;; inherit-input-method
+     t)))
+  (slime-interactive-eval (concat "(def " var-name " " expression ")")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key "\C-c\C-s\C-t" 'krb-clj-open-stacktrace-line)
@@ -912,6 +939,7 @@ the pre-existing package statements.
   (local-set-key [f4]           'krb-clj-test-run-all-tests-for-buffer)
   (local-set-key [f6]           'krb-clojure-replay-expression)
   (local-set-key (kbd "C-<f6>") 'krb-clojure-set-replay-expression)
+  (local-set-key (kbd "M-<f6>") 'krb-clojure-interactive-def-expression)
   (local-set-key [f7]           'krb-clojure-replay-inspect-expression)
   (local-set-key (kbd "C-<f7>") 'krb-clojure-set-replay-inspect-expression))
 
